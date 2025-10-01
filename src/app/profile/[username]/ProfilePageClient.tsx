238a1b487ff193ccd01a6f8a0c470b7e2c579b62
natsu2597 <atulrishi1997@gmail.com>
2025-10-02 02:13:22 +0530

    worling on implicit error

--- src/app/profile/[username]/ProfilePageClient.tsx
+++ src/app/profile/[username]/ProfilePageClient.tsx
@@ -181,7 +181,7 @@ function ProfilePageClient({isUserFollowing: isCurrentUserFollowing,user,posts,l
                    <TabsContent value="likes" className="mt-6">
             <div className="space-y-6">
               {posts.length > 0 ? (
-                posts.map((post) => <PostCard key={post.id} post={post} dbUserId={user.id} />)
+                posts.map((post : any) => <PostCard key={post.id} post={post} dbUserId={user.id} />)
               ) : (
                 <div className="text-center py-8 text-muted-foreground">No liked posts to show</div>
               )}
